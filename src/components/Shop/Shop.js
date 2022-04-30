cc8a076eb5f9e6ce98ec3c75633609d9be38e634
Mohasin Hossain <mohasinhossainrajib2@gmail.com>
2022-04-30 17:29:39 +0600

    Search players

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -6,11 +6,15 @@ import './Shop.css';
 const Shop = () => {
     const [players, setPlayers] = useState([]);
     const [cart, setCart] = useState([]);
+    const [displyPlayers, setDisplayPlayers] = useState([]);
 
     useEffect(() => {
         fetch('./fakeData.JSON')
         .then(res => res.json())
-        .then(data => setPlayers(data))
+        .then(data => {
+            setPlayers(data)
+            setDisplayPlayers(data)
+        })
     }, [])
 
     const handleAddToCart = (player) => {
@@ -28,25 +32,35 @@ const Shop = () => {
         setCart(deleteItem);   
     }
 
+    const handleChange = (event) => {
+        const searchText = event.target.value;
+        const matchedPlayer = players.filter(player => player.name.toLowerCase().includes(searchText.toLowerCase()));
+        setDisplayPlayers(matchedPlayer);
+    }
+
 
     return (
-        <div className='shop-container'>
-            <div className="players-container">
-                {
-                    players.map(player => <Player 
-                        handleAddToCart={handleAddToCart}
-                        key={player.id} 
-                        player={player}
-                        ></Player>)
-                }
+        <>
+            <div className="input-container">
+                <input onChange={handleChange} type="text" placeholder='Search player' />
             </div>
-            <div className="cart-container">
-                <div className="cart-items scrollbar-hidden">
-                    {cart.length ? <Cart handleDeleteItem={handleDeleteItem} cart={cart}></Cart> : null}
+            <div className='shop-container'>
+                <div className="players-container">
+                    {
+                        displyPlayers.map(player => <Player 
+                            handleAddToCart={handleAddToCart}
+                            key={player.id} 
+                            player={player}
+                            ></Player>)
+                    }
+                </div>
+                <div className="cart-container">
+                    <div className="cart-items scrollbar-hidden">
+                        {cart.length ? <Cart handleDeleteItem={handleDeleteItem} cart={cart}></Cart> : null}
+                    </div>
                 </div>
             </div>
-           
-        </div>
+        </>
     );
 };
 
